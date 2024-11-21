81d92992430ba37fecaec4e82e2dd91326d5da23
kavisperera <kavindayap57@gmail.com>
2024-11-22 01:30:48 +0530

    2 Commit

--- bill-management-system/src/app/app.component.ts
+++ bill-management-system/src/app/app.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { HomeComponent } from './home/home.component';
+import { PayBillComponent } from './pay-bill/pay-bill.component';
+import { ViewBillComponent } from './view-bill/view-bill.component';
+import { GenerateBillComponent } from './generate-bill/generate-bill.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, DashboardComponent, HomeComponent, PayBillComponent,ViewBillComponent,GenerateBillComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ bill-management-system/src/app/generate-bill/generate-bill.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-generate-bill',
+  imports: [],
+  templateUrl: './generate-bill.component.html',
+  styleUrl: './generate-bill.component.css'
+})
+export class GenerateBillComponent {
+
+}
